da2dc80732a0d16948f3bfb4391af24c2d23fd90
EnderYeekkay <nikita.kolobrodov.02@mail.ru>
2025-12-11 19:01:47 +0300

    - Yet one time warpFix has been fixed - Fixed vulnerability

---
+++ modules/warpFix.ts
@@ -0,0 +1,15 @@
+import { execSync, exec } from 'child_process'
+export function warpFix() {
+    try {
+        if (execSync('warp-cli tunnel host list').includes('api.github.com')) {
+        console.log('WarpFix is already installed')
+        } else {
+        execSync(`warp-cli tunnel host add "api.github.com"`)
+        execSync('sc stop "CloudflareWARP"')
+        setTimeout(() => exec('sc start "CloudflareWARP"'), 2000)
+        console.log('WarpFix has been installed.')
+    }
+    } catch (e) {
+        console.log('Cloudflare WARP not detected.')
+    }
+}
