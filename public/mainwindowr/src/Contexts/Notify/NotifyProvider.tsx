0f609a530c53e0affc8ef21490e56d3d77a079e8
EnderYeekkay <nikita.kolobrodov.02@mail.ru>
2026-02-07 12:01:06 +0300

    safe

--- public/mainwindowr/src/Contexts/Notify/NotifyProvider.tsx
+++ public/mainwindowr/src/Contexts/Notify/NotifyProvider.tsx
@@ -1,8 +1,11 @@
-import { createContext, Dispatch, SetStateAction, useEffect, useState } from "react";
+import { createContext, Dispatch, RefObject, SetStateAction, useEffect, useRef, useState } from "react";
 import Notify, { NotifyOptions, NotifyProps } from "./notify/notify.tsx";
 import NotifiesList from "./notifiesList/notifiesList.tsx";
 export interface NotifyContextType {
     sendNotify: (props: NotifyProps) => void
+    setVisibilityOfList: Dispatch<SetStateAction<boolean>>
+    visibilityOfList: boolean
+    visibilityOfListBtn: RefObject<HTMLDivElement>
 }
 const NotifyContext = createContext<NotifyContextType>(null) 
 export default NotifyContext
@@ -10,20 +13,38 @@ export default NotifyContext
 export function NotifyProvider({ children }: ContextProps) {
     const [notifyProps, setNotifyProps] = useState<NotifyProps>(null)
     const [notifies, setNotifies] = useState<NotifyOptions[]>([])
+    const [visibilityOfList, setVisibilityOfList] = useState<boolean>(false)
+    const visibilityOfListBtn = useRef<HTMLDivElement>(null)
+
     const sendNotify = (props: NotifyOptions, silent = false) => {
         const NotifyProps = {...props, id: Date.now()}
         if (!silent) setNotifyProps(notifyProps)
         setNotifies([...notifies, NotifyProps])
     }
     const clearNotify = () => setNotifyProps(null)
-    
+
+    useEffect(() => {
+        if (!visibilityOfListBtn.current?.style) return
+        
+        if (visibilityOfList) {
+            visibilityOfListBtn.current.style.opacity = '1'
+        } else {
+            visibilityOfListBtn.current.style.opacity = '.8'
+        }
+    }, [visibilityOfList])
+
     return <NotifyContext.Provider value={{ 
-        sendNotify
+        sendNotify,
+        setVisibilityOfList,
+        visibilityOfList,
+        visibilityOfListBtn
     }}>
         {children}
         {notifyProps &&
             <Notify {...{...notifyProps, clearNotify}} key={notifyProps.id}/>
         }
-        {NotifiesList()}
+        {visibilityOfList && 
+            <NotifiesList/>
+        }
     </NotifyContext.Provider>
 }
--- public/mainwindowr/src/Contexts/Notify/notifiesList/notifiesList.tsx
+++ public/mainwindowr/src/Contexts/Notify/notifiesList/notifiesList.tsx
@@ -2,7 +2,8 @@ import styles from './notifiesList.module.scss'
 
 export default function NotifiesList() {
     return <div className={`container ${styles.list}`}>
-		  <div className={styles.separator}>asdasdasdas</div>
-      <div>a</div>
+        <div className={styles.maintext}>Уведомления</div>
+        <div className={styles.separator}></div>
+        <div>a</div>
     </div>
 }
