3706e0b612bbb7e34f725440dd64be640c8e4cbc
EnderYeekkay <114084689+EnderYeekkay@users.noreply.github.com>
2026-02-07 12:01:06 +0300

    safe

--- public/mainwindowr/src/Components/header/notifiesListBtn.tsx
+++ public/mainwindowr/src/Components/header/notifiesListBtn.tsx
@@ -1,6 +1,15 @@
+import { useContext } from 'react'
 import styles from './header.module.scss'
+import NotifyContext from '../../Contexts/Notify/NotifyProvider.tsx'
 export default function() {
-    return <div className={styles.header_btn} id={styles.notifies_list} onClick={() => {}}>
+    const { setVisibilityOfList, visibilityOfList, visibilityOfListBtn } = useContext(NotifyContext)
+
+    return <div
+        ref={visibilityOfListBtn}
+        className={styles.header_btn}
+        id={styles.notifies_list}
+        onClick={() => setVisibilityOfList(!visibilityOfList)}
+    >
         <img className={styles.header_btn_ico} src="../notifies.png" alt=""/>
     </div>
 }
