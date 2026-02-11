ea712279561f1ecf001ba5f51dc1918094c605fe
EGSP <repsbro@gmail.com>
2026-02-11 21:20:36 +0300

    refactor: добавлены вкладки (tabs, tab, tab panel)

--- public/mainwindowr/src/Components/SettingsFrame.tsx
+++ public/mainwindowr/src/Components/SettingsFrame.tsx
@@ -1,16 +1,46 @@
 import { useContext } from "react";
 import Button from "./button/button.tsx";
 import { FrameContext } from "../Contexts/FrameContext.tsx";
-import { Column } from "./Structure/Alignment.tsx";
+import { Column, Row } from "./Structure/Alignment.tsx";
+import { Tab, TabPanel, Tabs } from "./Tab/Tab.tsx";
 
 export default function SettingsFrame() {
+    type AllowedTabs = 'general' | 'ipset' | 'strategies' | 'debug';
     const frameContext = useContext(FrameContext);
     return (
-        <>
+        <Row>
+            <Tabs<AllowedTabs> defaultTab="general">
+                <Row>
+                    <Column>
+                        <Tab<AllowedTabs> description="Основные" tabName="general" />
+                        <Tab<AllowedTabs> description="IPSET" tabName="ipset" />
+                        <Tab<AllowedTabs> description="Стратегии" tabName="strategies" />
+                        <Tab<AllowedTabs> description="Отладка" tabName="debug" />
+                    </Column>
+
+                    <Column>
+                        <TabPanel<AllowedTabs> tabName="general">
+                            <p>Основные</p>
+                        </TabPanel>
+                        <TabPanel<AllowedTabs> tabName="ipset">
+                            <p>IPSET</p>
+                        </TabPanel>
+                        <TabPanel<AllowedTabs> tabName="strategies">
+                            <p>Стратегии</p>
+                        </TabPanel>
+                        <TabPanel<AllowedTabs> tabName="debug">
+                            <p>Отладка</p>
+                        </TabPanel>
+                    </Column>
+                </Row>
+            </Tabs>
+
+
             <Column>
                 <p>Settings</p>
                 <Button label="Назад" action={() => frameContext.setFrame('home')} />
             </Column>
-        </>
+
+        </Row>
     )
 }
